0614317fd4706d8e5147494766ef209623304f40
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-01-28 23:42:03 +0530

    create multiple value annotation for achieve component scan feature

--- dependency_injection_framework/src/main/java/com/java_reflection/annotation/AutoComponentScan.java
+++ dependency_injection_framework/src/main/java/com/java_reflection/annotation/AutoComponentScan.java
@@ -1,11 +1,11 @@
 package com.java_reflection.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(AutoComponentScans.class)
 public @interface AutoComponentScan {
+    public String value();
+
 }
